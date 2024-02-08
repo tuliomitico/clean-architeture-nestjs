6f1b95d4eb1fb71caa85d54b92671b34e6c03290
tuliomitico <tulioneedforspeed@gmail.com>
2024-02-08 17:23:17 -0300

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { envSchema } from './env'
       isGlobal: true,
       validate: (env) => envSchema.parse(env),
     }),
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
