45915e535c974f1ec42e46fe5d844c0c71c1a66e
tuliomitico <tulioneedforspeed@gmail.com>
2024-02-08 17:49:49 -0300

    feat: add authenticate controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,9 +9,14 @@ import { Env } from 'src/env'
     PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
+      global: true,
       useFactory(config: ConfigService<Env, true>) {
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
         return {
-          secret: config.get('JWT_SECRET', { infer: true }),
+          signOptions: { algorithm: 'RS256', expiresIn: '1d' },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
         }
       },
     }),
