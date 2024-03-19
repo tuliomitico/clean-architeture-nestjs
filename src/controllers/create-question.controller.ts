5fd85f5eda1aa74b658eb0f00922e877d8ac2f0d
tuliomitico <tulioneedforspeed@gmail.com>
2024-03-19 13:57:18 -0300

    feat: add fetch questions controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -34,7 +34,7 @@ export class CreateQuestionController {
       data: {
         title,
         content,
-        slug: 'asd',
+        slug,
         authorId: userId,
       },
     })
---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidatioPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(@Query('page', queryValidatioPipe) page: PageQueryParamSchema) {
+    const perPage = 1
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+    return { questions }
+  }
+}
