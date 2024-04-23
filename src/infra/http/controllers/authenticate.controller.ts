4c7e2c26fc934b2f6f4dd5230e56034a9204b99c
tuliomitico <tulioneedforspeed@gmail.com>
2024-04-23 20:50:03 -0300

    feat: add prisma forum repositories

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, UsePipes } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { compare } from 'bcryptjs'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
-import { PrismaService } from '@/infra/prisma/prisma.service'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { z } from 'zod'
 
 const authenticateBodySchema = z.object({
