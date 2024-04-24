e811517d6fb087b3fa7b0fd20a170edd0a887272
tuliomitico <tulioneedforspeed@gmail.com>
2024-04-23 21:19:22 -0300

    fix: add repos in the db module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,5 +1,30 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-question-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
 
-@Module({ providers: [PrismaService], exports: [PrismaService] })
+@Module({
+  providers: [
+    PrismaService,
+    PrismaAnswersRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaQuestionsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaQuestionCommentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaAnswersRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaQuestionsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaQuestionCommentsRepository,
+  ],
+})
 export class DatabaseModule {}
