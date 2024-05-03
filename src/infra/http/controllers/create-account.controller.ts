7d524ae6e0248b0e6fedc27d38ebf449f776bece
tuliomitico <tulioneedforspeed@gmail.com>
2024-05-03 20:58:40 -0300

    refact: add auth and register use case in controllers

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -28,25 +28,5 @@ export class CreateAccountController {
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
-
-    const userWithSameEmail = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
-
-    if (userWithSameEmail) {
-      throw new ConflictException('User with same email already exists')
-    }
-
-    const hashedPassword = await hash(password, 8)
-
-    await this.prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-      },
-    })
   }
 }
