d89f2590e3b1983521f194776bc2139dc58fab88
tuliomitico <tulioneedforspeed@gmail.com>
2024-09-03 20:26:17 -0300

    feat: add read notification controller

---
+++ src/infra/http/controllers/read-notification.controller.ts
@@ -0,0 +1,32 @@
+import {
+  BadRequestException,
+  Controller,
+  HttpCode,
+  Param,
+  Patch,
+} from '@nestjs/common'
+import { ReadNotificationUseCase } from '@/domain/notification/application/use-cases/read-notification'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+
+@Controller('/notifications/:notificationId/read')
+export class ReadNotificationController {
+  constructor(private readNotification: ReadNotificationUseCase) {}
+
+  @Patch()
+  @HttpCode(204)
+  async handle(
+    @Param('notificationId') notificationId: string,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { sub: recipientId } = user
+    const result = await this.readNotification.execute({
+      notificationId,
+      recipientId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
