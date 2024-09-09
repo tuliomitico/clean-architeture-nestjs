19292e3b65776dd3fff4a55516bc125e7cd1746d
tuliomitico <tulioneedforspeed@gmail.com>
2024-09-09 20:59:35 -0300

    feat: add redis cache service and repo

---
+++ src/infra/cache/redis/redis.service.ts
@@ -0,0 +1,16 @@
+import { EnvService } from '@/infra/env/env.service'
+import { Injectable, OnModuleDestroy } from '@nestjs/common'
+import { Redis } from 'ioredis'
+
+@Injectable()
+export class RedisService extends Redis implements OnModuleDestroy {
+  constructor(envService: EnvService) {
+    super(Number(envService.get('REDIS_PORT')), envService.get('REDIS_HOST'), {
+      db: Number(envService.get('REDIS_DB')),
+    })
+  }
+
+  onModuleDestroy() {
+    return this.disconnect()
+  }
+}
